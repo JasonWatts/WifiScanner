af0fb3e80134233f1874a13bda17010377c5e9b8
jamesSolum <james.n.solum@gmail.com>
2017-08-31 16:09:01 -0700

    started the selenium script

---
+++ scanWifi.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = "/Users/jamessolum/Downloads/chromedriver"
+speedTest = "http://www.speedtest.net/"
+survey = "https://docs.google.com/forms/d/e/1FAIpQLSfFh56GV63_CJWpbNEc-K41p6EgdtztOo83Wz9ZssH2i5V9bw/viewform?c=0&w=1"
+meraki = "
+
