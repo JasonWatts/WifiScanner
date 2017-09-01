8fc2678b6ee0caaa398fe16e9ec975de6ce45fb3
jamesSolum <james.n.solum@gmail.com>
2017-08-31 19:30:03 -0700

    hello

--- scanWifi.py
+++ scanWifi.py
@@ -6,11 +6,14 @@ import pyspeedtest
 
 class Scanner(object):
     def __init__(self):
+        self.avgPing = 0
+        self.avgDownload = 0
+        self.avgupload =0
         self.listPing = []
         self.listDownload = []
         self.listUpload = []
 
-    def scan():
+    def scan(self):
         try:
             st = pyspeedtest.SpeedTest()
             ping = st.ping()
@@ -21,51 +24,50 @@ class Scanner(object):
             listDownload.append(download)
             listUpload.append(upload)
         except:
-            print("Error")
+            print("Error with Scan")
 
     def average(aList):
-		total = 0
-		for num in aList:
-			total = total + num 
-		return total/len(aList)
+        total = 0
+        for num in aList:
+            total = total + num 
+        return total/len(aList)
 
-
-    def fullScan(num):
-    	for n in range(num):
-    		scan()
-
-    	print("Ping: ", average(listPing))
-    	print("Download: ", average(listDownload))
-    	print("Upload: ", average(listUpload))
-
-
-
-
-
-
-
-
-
+    def fullScan(self, num):
+        for n in range(num):
+            self.scan()
+        self.avgPing = (average(listPing))
+        print("Ping: ", self.avgPing)
+        self.avgDownload = (average(listDownload))
+        print("Download: ", self.avgDownload)
+        self.avgUpload = (average(listUpload))
+        print("Upload: ", self.avgUpload)
 
 def main():
     # Requires Editing
     user = "TYPE YOUR USER HERE"
     device = "TYPE YOUR DEVICE HERE"
     driver = "/Users/jamessolum/git/WifiScanner/chromedriver"
-    
+    dorm = "GLC"
+   
     # Helpful Sites
     speedTest = "http://www.beta.speedtest.net/"
     survey = "https://docs.google.com/forms/d/e/1FAIpQLSfFh56GV63_CJWpbNEc-K41p6EgdtztOo83Wz9ZssH2i5V9bw/viewform?c=0&w=1"
     meraki = "my.meraki.com"
-    dorm = "GLC"
-    
+    scanSize = 1
+
     #User input
     print("Wifi Scanner")
     print("User: ", user)
     print("Device: ", device)
     location = dorm + "-" + str(input("Room number:"))
+  
+    scanner = Scanner()
+    scanner.fullScan(scanSize)
     
-     
+    browser = webdriver.Chrome(driver)
+    browser.get(meraki)
+    deviceName = browser.find_element_by_name('device_name').text
+    print("Device name: ", deviceName)
 
 
 if __name__ == "__main__":
