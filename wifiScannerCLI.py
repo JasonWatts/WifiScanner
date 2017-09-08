ca87467ddef53dc75da7de74c2a291b1d3794f53
jamesSolum <james.n.solum@gmail.com>
2017-09-08 12:22:53 -0700

    scans wifi and fails at parsing html

---
+++ wifiScannerCLI.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+import pyspeedtest
+from lxml import html
+import requests
+
+def main():
+    # Requires Editing
+    user = "jsolum"
+    device = "Mac desktop or laptop"
+    driver = "/Users/jamessolum/git/WifiScanner/chromedriver"
+    dorm = "GLC"
+   
+    # Helpful Sites
+    meraki = 'http://my.meraki.com/#connection'
+
+    #User input
+    print("Wifi Scanner")
+    print("User: ", user)
+    print("Device: ", device)
+    location = dorm + "-" + str(input("Room number: "))
+   
+    # Speed Test
+    st = pyspeedtest.SpeedTest()
+    ping = round(st.ping(), 2)
+    download = round(st.download() / 1000000, 2)
+    upload = round(st.upload() / 1000000, 2)
+
+    print("Ping: ", ping)
+    print("Download: ", download)
+    print("Upload: ", upload)
+
+    # WAP Data
+    page = requests.get(meraki)
+    content = html.fromstring(page.content)
+    device = content.xpath('//*[@id="device_name"]/text()')
+    print("Device: ", device)
+
+if __name__ == "__main__":
+    main()
+
+    
+
+
