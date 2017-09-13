34298c68748f1cfd65a160de56a76173620e0dce
jamesSolum <james.n.solum@gmail.com>
2017-09-12 20:53:56 -0700

    retrieving url on setup

--- google_io.py
+++ google_io.py
@@ -2,13 +2,14 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-SHEET_KEY = "1pTHKzoJyq2UZMBQduGyoRLtxdzcu4ITEvBv48TEaKwE"
-
+with open('url.txt', 'r') as myfile:
+    SHEET_KEY=myfile.read()
+print(SHEET_KEY)
 scope = ['https://spreadsheets.google.com/feeds']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
 gc = gspread.authorize(credentials)
 
-sheet = gc.open_by_key(SHEET_KEY).sheet1
+sheet = gc.open_by_url(SHEET_KEY).sheet1
 
 row = ["hello", "my", "name", "is", "James", 4]
 sheet.insert_row(row, sheet.row_count+1)
--- setup.sh
+++ setup.sh
@@ -5,3 +5,6 @@ while read p; do
   python3 -m pip install $p
 done <requirements.txt
 
+printf 'Please provide Google Spread sheet url:'
+read url
+echo $url > url.txt
